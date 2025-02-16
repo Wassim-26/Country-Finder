12ab819ff216fe9aaf2b71719d5729a44ce9e518
Wassim-26 <abderahmanewassim652@gmail.com>
2025-02-17 00:02:03 +0100

    Add button of dark and light mode and some modification

--- src/App.js
+++ src/App.js
@@ -2,12 +2,31 @@ import "./App.css";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navebar/Navbar";
 import SearchSection from "./components/SearchSection/Search";
+import React, { useState, useEffect } from "react";
 function App() {
+  const [darkMode, setDarkMode] = useState(
+    localStorage.getItem("darkMode") === "true"
+  );
+
+  useEffect(() => {
+    localStorage.setItem("darkMode", darkMode);
+  }, [darkMode]);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
   return (
-    <div className="App">
-      <Navbar />
-      <SearchSection />
-      <Hero />
+    <div className="app">
+      <div className={darkMode ? "dark-mode" : "light-mode"}>
+        <Navbar />
+        <div className="butt-dark-light-container">
+          <button className="butt-dark-light" onClick={toggleDarkMode}>
+            {darkMode ? "Turn Off Dark Mode" : "Enable Dark Mode"}
+          </button>
+        </div>
+        <SearchSection />
+        <Hero />
+      </div>
     </div>
   );
 }
